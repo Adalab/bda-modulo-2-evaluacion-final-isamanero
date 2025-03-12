57e445890a58be67af2e1fe1eec8be49245ceedc
Isabel <isaw911@gmail.com>
2025-03-12 20:44:58 +0100

    sin completar

--- BONUS.sql
+++ BONUS.sql
@@ -3,13 +3,29 @@
 debe mostrar el nombre y apellido de los actores y el número de películas en las que han actuado juntos.*/
 
  -- Para este ejercicio, necesitamos coincidencias de actor_id con film_id.
+ -- Entiendo que hay que hacer dos grupos de actores, para ir comparando uno con todos los demás
+
  -- Utilizando la tabla film_actor:
  
- SELECT actor_id, film_id
-	FROM film_actor
-	WHERE actor_id = 1;
+SELECT a.actor_id, a.first_name, a.last_name, a2.actor_id, a2.first_name, a2.last_name,
+ COUNT(f.film_id)
+	FROM actor a
+	INNER JOIN actor a2
+	ON  a2.actor_id = a.actor_id
+	INNER JOIN film_actor f
+	ON a.actor_id = f.actor_id
+	WHERE  a.actor_id < a2.actor_id
+	GROUP BY a2.actor_id,  a.actor_id
+	HAVING count(*) > 1
+	ORDER BY a.actor_id, a2.actor_id;
+ 
+ 
+ 
+ select count(*)
+ from film_actor
+ where actor_id in (1, 2)
+ 
     
--- Entiendo que hay que hacer dos grupos de actores, para ir comparando uno con todos los demás
 
 
     
