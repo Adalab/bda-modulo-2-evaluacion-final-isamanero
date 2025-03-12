f1e60f368315a3ea921dd86bcb7571c1568a9491
Isabel <isaw911@gmail.com>
2025-03-12 13:54:55 +0100

    guardado

---
+++ BONUS.sql
@@ -0,0 +1,9 @@
+-- BONUS --   
+/* #### 25.Encuentra todos los actores que han actuado juntos en al menos una película. La consulta
+debe mostrar el nombre y apellido de los actores y el número de películas en las que han actuado juntos.*/
+
+ -- Para este ejercicio, necsitamos coincidencias de actor_id con film_id.
+ -- Utilizando la tabla film_actor:
+ 
+ SELECT *
+	FROM film_actor;
